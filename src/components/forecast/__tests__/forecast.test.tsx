15dca7e8dbe649c90f1c6129e93fd7c64b6f0014
Bittu <bittu1028@gmail.com>
2022-09-13 12:34:01 +1000

    Added unit test

--- src/components/forecast/__tests__/forecast.test.tsx
+++ src/components/forecast/__tests__/forecast.test.tsx
@@ -1,4 +1,4 @@
-// __tests__/searchBox.test.js
+// __tests__/forecast.test.tsx
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
--- src/container/HomePage/__tests__/HomePage.test.tsx
+++ src/container/HomePage/__tests__/HomePage.test.tsx
@@ -1,10 +1,29 @@
-// __tests__/searchBox.test.js
+// __tests__/HomePage.test.tsx
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { Provider } from 'react-redux';
+import HomePage from '../HomePage';
+import store from '../../../reducers/store';
+
+const renderHomePage = () =>
+  render(
+    <Provider store={store}>
+      <HomePage />
+    </Provider>
+  );
 
 describe('HomePage', () => {
   test('renders current homepage component', async () => {
-    expect(true).toBeTruthy();
+    renderHomePage();
+    const titleInput = screen.getByPlaceholderText('Search for city');
+    expect(titleInput).toBeInTheDocument();
+  });
+});
+
+describe('HomePage', () => {
+  test('renders current homepage component', async () => {
+    const { asFragment } = renderHomePage();
+    expect(asFragment).toMatchSnapshot();
   });
 });
