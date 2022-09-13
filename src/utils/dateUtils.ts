60e76b80afa572f99c3279a1ca98e33e2e19ac02
Bittu <bittu1028@gmail.com>
2022-09-14 02:42:18 +1000

    Fixed date utils

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -2,5 +2,7 @@
 
 export const isGreaterThanCurrentDate = (currentDate:Date, time: number) => {
     const weatherDate = new Date(time * 1000);
+    currentDate.setHours(0,0,0,0);
+    weatherDate.setHours(0,0,0,0);
     return Math.floor(weatherDate.getTime() / 86400000) > Math.floor(currentDate.getTime() / 86400000);
 }
